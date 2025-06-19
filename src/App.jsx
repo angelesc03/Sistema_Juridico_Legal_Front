ebcbbb181fd42ce95d0ee5b03a759627f3a20403
angelesc03 <angelhh1903@gmail.com>
2025-06-19 00:00:48 -0600

    Modificación de App.jsx para usar Hashmap

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import RegisterPage from './components/RegisterPage';
 import Home from './components/home';
 import LoginPage from './components/login';
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/registro" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/registro" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
   );
 }
 
